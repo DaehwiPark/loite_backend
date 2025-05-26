021ca8030f2388554d7621157bee95b21047a2a8
DESKTOP-9K23CHS\np <solfany1999@gmail.com>
2025-05-26 09:35:55 +0900

    build: 초기 세팅

---
+++ src/main/java/com/boot/loite_ms_back/LoiteMsBackApplication.java
@@ -0,0 +1,13 @@
+package com.boot.loite_ms_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoiteMsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoiteMsBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boot/loite_ms_back/LoiteMsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.loite_ms_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoiteMsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
