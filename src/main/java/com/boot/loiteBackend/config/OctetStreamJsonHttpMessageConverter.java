f8ca5045940c737a1b8d592b0297faedded05080
solfany <solfany1999@gmail.com>
2025-09-04 10:47:08 +0900

    feat: swagger API file 등록시 발생하는 오류 수정
    - OctetStreamJsonHttpMessageConverter 생성 /  // APPLICATION_OCTET_STREAM 을 JSON으로 처리 가능하게 등록

---
+++ src/main/java/com/boot/loiteBackend/config/OctetStreamJsonHttpMessageConverter.java
@@ -0,0 +1,21 @@
+package com.boot.loiteBackend.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OctetStreamJsonHttpMessageConverter extends AbstractJackson2HttpMessageConverter {
+
+    public OctetStreamJsonHttpMessageConverter(ObjectMapper objectMapper) {
+        // APPLICATION_OCTET_STREAM 을 JSON으로 처리 가능하게 등록
+        super(objectMapper, MediaType.APPLICATION_OCTET_STREAM);
+    }
+
+    @Override
+    protected boolean canWrite(MediaType mediaType) {
+        // 쓰기는 필요 없으니 false
+        return false;
+    }
+}
